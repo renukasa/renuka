78d24172ea9b59e132c7357193f7162d4585f471
renukasa <renukasa@renukasa-w8.cybage.com>
2019-06-10 13:46:18 +0530

    First commit

---
+++ arid/src/main/java/gid/arid/App.java
@@ -0,0 +1,13 @@
+package gid.arid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
